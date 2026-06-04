786a6fc1a07967137658897039f5e67cd5034512
LiuGaoyong <liugaoyong_88@163.com>
2026-06-04 18:39:28 +0800

    update more helper function, and test passed

--- src/graphatoms/system/_graph/__init__.py
+++ src/graphatoms/system/_graph/__init__.py
@@ -1,6 +1,11 @@
 """The graph representation of the system."""
+# ruff: noqa: F401
 
-from ._bonds import BondGraph
+from ._bonds import (
+    DEFAULT_WH_HASH_DEPTH,
+    DEFAULT_WH_HASH_SIZE,
+    BondGraph,
+)
 from ._sysGraph import SysGraph
 
 __all__ = ["BondGraph", "SysGraph"]
